5d85353fee1f98ce016a3a94a9a29a335e3bec3d
LevonGukasyanArsenovich <36114459+LevonGukasyanArsenovich@users.noreply.github.com>
2018-02-08 22:05:22 +0100

    Rename ejercicio-condicion-chunga.py to ejercicio-edad-3.py

---
+++ .gitignore/ejercicio-edad-3.py
@@ -0,0 +1,10 @@
+# CODING : UTF8
+print "Buenas"
+numero = input ("Dime un numero al azar=")
+if (numero%2==0) or (numero>=-10 and numero<=40) or (numero<0) :
+	print "CORRECT"
+else:
+	print "ERROR!"
+	print "ERROR!"
+    
+    
