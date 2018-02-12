94918612e43d5c6bc380c690939fc54b8486f97b
LevonGukasyanArsenovich <36114459+LevonGukasyanArsenovich@users.noreply.github.com>
2018-02-12 21:55:56 +0100

    Create Ejercicio-mayor.py

---
+++ .gitignore/Ejercicio-mayor.py
@@ -0,0 +1,11 @@
+# coding : utf8
+
+A = input("dime un numero=")
+B = input("dime un numero=")
+if A > B :
+	print A,"Es mayor que",B
+
+elif B > A :
+	print B,"Es mayor que",A
+else :
+	print "Son iguales"
