b07250bf668418868d85d673de2795a3a39108e1
LevonGukasyanArsenovich <36114459+LevonGukasyanArsenovich@users.noreply.github.com>
2018-03-05 21:24:48 +0100

    Create ejerciciocontar.py

---
+++ .gitignore/ejerciciocontar.py
@@ -0,0 +1,15 @@
+# coding: utf8
+import time
+salir  = "N"
+numero = 1 
+
+while ( salir=="N" ):
+	# Hago cosas
+	print "informe año:",numero
+    
+	# Incremento
+	numero = numero +1
+	time.sleep(0.1)
+	# Activo indicador de salida si toca
+	if ( numero >50): # Condición de salida
+    	    salir = "S"
