a18986073e538f2cc6c7a79f68f3ab463ab07894
LevonGukasyanArsenovich <36114459+LevonGukasyanArsenovich@users.noreply.github.com>
2018-03-05 21:24:27 +0100

    Create bucle.py

---
+++ .gitignore/bucle.py
@@ -0,0 +1,13 @@
+# coding: utf8
+salir  = "N"
+numero = 2010 
+
+while ( salir=="N" ):
+	# Hago cosas
+	print "informe año:",numero
+
+	# Incremento
+	numero = numero +1
+	# Activo indicador de salida si toca
+	if ( numero >2016): # Condición de salida
+    	    salir = "S"
