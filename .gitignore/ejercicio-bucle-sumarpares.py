b14912cf2f8c69dcd37106fc6a5e66acc98085fc
LevonGukasyanArsenovich <36114459+LevonGukasyanArsenovich@users.noreply.github.com>
2018-03-15 00:07:16 +0100

    Update ejercicio-bucle-sumarpares.py

--- .gitignore/ejercicio-bucle-sumarpares.py
+++ .gitignore/ejercicio-bucle-sumarpares.py
@@ -2,22 +2,25 @@
 # Levon Gukasyan
 
 # Inicializaciones
-salir  = "N"
-suma   = 1
-maximo = input ("Digame numero:") 
-suma1  = 0
+salir   = "N"
+suma1   = 1
+maximo  = 5 
+suma    = 0
 
 if maximo <= 0 :
-	print "Error 404"
+	print "Error!"
 
 while ( salir=="N" ):
 	# Hago cosas
-	print suma
-
+	if suma1%2==0:
+		print suma1,
+        if suma1 == maximo -2:
+			print "+",
+        
 	# Incremento
-	suma1 = suma + suma1
-	suma  = suma + 1
+        suma = suma + suma1
+        suma1  = suma1 + 1
 	# Activo indicador de salida si toca
-	if ( suma > maximo): # Condición de salida
-		print suma1
+	if ( suma1 > maximo): # Condición de salida
 		salir = "S"
+print "=", suma1
