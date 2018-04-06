9e9a39adca17f378dae521602a8a1746bf544104
LevonGukasyanArsenovich <36114459+LevonGukasyanArsenovich@users.noreply.github.com>
2018-04-06 22:30:00 +0200

    Update luckysky.py

--- .gitignore/luckysky.py
+++ .gitignore/luckysky.py
@@ -36,35 +36,35 @@ if trabajador =="S" :
 	if cargo == 1 and desc == "S": 
 		print "--Azafata De Halcon Milenario tiene un 45% desc."
 		print "--PRECIO TOTAL:",Decimal(500-(500*45/100)),"$"
-	
+		print "GRACIAS POR SU VISITA"
 	if cargo == 1 and desc == "N":
 		print "--Azafata De Halcon Milenario tiene un 10% desc." 
 		print "--PRECIO TOTAL:",Decimal(500-(500*10/100)),"$"
-		
+		print "GRACIAS POR SU VISITA"
 	if cargo == 2 and desc == "S": 
 		print "--HAN SOLO PILOTO tiene un 95% desc."
 		print "--PRECIO TOTAL:",Decimal(500-(500*95/100)),"$"
-	
+		print "GRACIAS POR SU VISITA"
 	if cargo == 2 and desc == "N":
 		print "--HAN SOLO PILOTO tiene un 60% desc." 
 		print "--PRECIO TOTAL:",Decimal(500-(500*60/100)),"$"
-		
+		print "GRACIAS POR SU VISITA"
 	if cargo == 3 and desc == "S": 
 		print "--HAN SOLO PILOTO tiene un 95% desc."
 		print "--PRECIO TOTAL:",Decimal(500-(500*95/100)),"$"
-	
+		print "GRACIAS POR SU VISITA"
 	if cargo == 3 and desc == "N":
 		print "--HAN SOLO PILOTO tiene un 60% desc." 
 		print "--PRECIO TOTAL:",Decimal(500-(500*60/100)),"$"
-		
+		print "GRACIAS POR SU VISITA"
 	if (cargo == 4 or cargo == 5) and desc == "S": 
 		print "--Los CHEWBACCAS Y los Carga maletas teneis un 40% desc."
 		print "--PRECIO TOTAL:",Decimal(500-(500*40/100)),"$"
-	
+		print "GRACIAS POR SU VISITA"
 	if (cargo == 4 or cargo == 5) and desc == "N":
 		print "--Los CHEWBACCAS Y los Carga maletas teneis un 5% desc." 
 		print "--PRECIO TOTAL:",Decimal(500-(500*5/100)),"$"
-		
+		print "GRACIAS POR SU VISITA"
 	print "GRACIAS POR SU VISITA"
 		
 elif  trabajador == "N" :
@@ -73,27 +73,28 @@ elif  trabajador == "N" :
 #AQUI PREGUNTA LA EDAD DEL CLIENTE
 	edad = input("--Introduzca su edad: ")
 	 
-
-	if edad <=3 :
+	if edad <0 :
+		print "EDAD MAL INTRODUCIDA"
+	if edad >=0 and edad <=3 :
 		print "El billete es GRATIS"
-
-	if edad >3 and edad <10 : 
+		print "GRACIAS POR SU VISITA"
+	if edad >3 and edad <=10 : 
 		print "--DESCUENTO EWOK 80%"
 		print "--PRECIO TOTAL",Decimal(500-(500*80/100)),"$"
-		
-	if 	edad >=10 and edad <=16 : 
+		print "GRACIAS POR SU VISITA"
+	if 	edad >10 and edad <=16 : 
 		print "--DESCUENTO PADAWAN 15%"
 		print "--PRECIO TOTAL",Decimal(500-(500*15/100)),"$"
-	
+		print "GRACIAS POR SU VISITA"
 	if 	edad >16 and edad <=60 : 
 		print "--JEDI NORMAL Usted no tiene descuento"
 		print "--PRECIO TOTAL: 500$"
-		
+		print "GRACIAS POR SU VISITA"
 	if 	edad >60 : 
 		print "--DESCUENTO YODA(40%)"
 		print "--PRECIO TOTAL",Decimal(500-(500*40/100)),"$"
+		print "GRACIAS POR SU VISITA"
 		
-	print "GRACIAS POR SU VISITA"
 		
 elif trabajador == "LO" :#SI ES DEL LADO OSCURO LE PREGUNTA SI ES LORD SITH O DARTH VADER
 	print """   
@@ -112,20 +113,19 @@ elif trabajador == "LO" :#SI ES DEL LADO OSCURO LE PREGUNTA SI ES LORD SITH O DA
 	if cargo2 == 1 and desc2 =="S" :
 		print "--LORD SITH TIENE UN DESCUENTO DEL 45%"
 		print "--PRECIO TOTAL",Decimal(500-(500*45/100)),"$"
-		
+		print "GRACIAS POR SU VISITA"
 	if cargo2 == 1 and desc2 =="N" :
 		print "--LORD SITH TIENE UN DESCUENTO DEL 10%"
 		print "--PRECIO TOTAL",Decimal(500-(500*10/100)),"$"
-		
+		print "GRACIAS POR SU VISITA"
 	if cargo2 == 2 and desc2 =="S" :
 		print "--DARTH VADER TIENE UN DESCUENTO DEL 65%"
 		print "--PRECIO TOTAL",Decimal(500-(500*65/100)),"$"
-		
+		print "GRACIAS POR SU VISITA"
 	if cargo2 == 2 and desc2 =="N" :
 		print "--DARTH VADER TIENE UN DESCUENTO DEL 20%"
 		print "--PRECIO TOTAL",Decimal(500-(500*20/100)),"$"
-	print "GRACIAS POR SU VISITA"
+		print "GRACIAS POR SU VISITA"
 	
 else:
 	print "Error"
-
